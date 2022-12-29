743963f7ef2b8adc1d09bb07e05e1ae117e1759f
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-29 15:58:26 +0700

    Update c1.sh

--- c1.sh
+++ c1.sh
@@ -2,14 +2,15 @@
 pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
-    echo $pid;
+    #echo $pid;
     if pstree $pid | grep python
     then
-        echo "OK";
+        echo "Worker $name is working";
     else
-        echo "NOT OK";
+        echo "Worker $name is not working";
 	name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
+        #/usr/bin/screen -SX $pid quit;
 	echo $name;
-	echo $data;
+	screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
     fi
 done
