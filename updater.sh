c6f55a103945fbfc2da45898651d8d77c5b7eac7
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-29 17:28:34 +0700

    Update updater.sh
    Add sleep

--- updater.sh
+++ updater.sh
@@ -5,5 +5,7 @@ do
     #echo $pid;
     name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
     echo "Restart worker $name";
-    screen -r $name -X stuff '^C && python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+    screen -r $name -X stuff $'\003'
+    sleep(15)
+    screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
 done
