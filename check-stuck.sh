5a2ba3315cdfb6f3ca761e97ea5521455ad53db7
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-30 13:04:42 +0700

    Update check-stuck.sh

--- check-stuck.sh
+++ check-stuck.sh
@@ -3,10 +3,17 @@ pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
     #echo $pid;
-    screen -X -S $name hardcopy log.txt
     name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
-    echo "Restart worker $name";
-    screen -r $name -X stuff $'\003'
-    sleep 15
-    screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+    screen -X -S $name hardcopy log.txt
+    if grep -r "COLLECT DATA" log.txt
+    then
+        echo "Restart worker $name";
+        screen -r $name -X stuff $'\003'
+        sleep 15
+        screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+    else
+        echo "Worker $name running perfectly";
+    fi
+    
+        
 done
