7e9fd9cf5b4fc6972d5efbaa1d15abde5efe1955
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-30 16:48:54 +0700

    Create stopper.sh

---
+++ stopper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
+for pid in ${pidList[@]};
+do
+    #echo $pid;
+    name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
+    echo "Restart worker $name";
+    screen -r $name -X stuff $'\003'
+    screen -r $name -X stuff $'\003'
+    screen -r $name -X stuff $'\003'
+done
+
