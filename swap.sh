9ba1d63e665164544978db67a21735fdbe2ba377
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-31 09:59:56 +0700

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fallocate -l 30G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
