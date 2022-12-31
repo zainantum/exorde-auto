fba760f0534008c7095f20ffbb4054b261a83eb0
Zain Antum <115614618+zainantum@users.noreply.github.com>
2022-12-31 10:27:49 +0700

    Update createScreen.sh

--- createScreen.sh
+++ createScreen.sh
@@ -4,7 +4,7 @@ for name in "${array[@]}"
 do
         echo "create screen $name"
         screen -dm $name
-        sleep 1
+        sleep 3
         screen -r $name -X stuff 'cd '${name}' && conda activate exorde-env && python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
 done
 
