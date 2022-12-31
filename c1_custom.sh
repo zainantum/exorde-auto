71ef3a22e8d6959b2987fbdc6440fb5ad8994c33
Zain Antum <115614618+zainantum@users.noreply.github.com>
2023-01-01 00:52:24 +0700

    Update c1_custom.sh

--- c1_custom.sh
+++ c1_custom.sh
@@ -2,8 +2,13 @@
 array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
 for name in "${array[@]}"
 do
-        id = $(screen -dm -S $name & echo $!)
-        pid = tail -n1 $id
-        echo "create $id screen $pid $name"
-        sleep 3
+        tid=$(screen -dm -S $name & echo $!)
+        if pstree $tid | grep python
+    then
+        echo "Worker $name is working";
+    else
+        echo "Worker $name is not working";
+	      echo $name;
+	      screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+    fi
 done
