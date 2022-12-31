ceba99ed88e4c8a90a9a4e352c8132deb2049b77
Zain Antum <115614618+zainantum@users.noreply.github.com>
2023-01-01 00:37:49 +0700

    Create c1_custom.sh

---
+++ c1_custom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
+for name in "${array[@]}"
+do
+        echo "create screen $name"
+        screen -dm $name
+        sleep 3
+        screen -r $name -X stuff 'cd '${name}' && conda activate exorde-env && python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+done
