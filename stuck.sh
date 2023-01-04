2c2930b49b2a0b19215d7a1b997f2ea249bfb820
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-04 19:06:33 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -6,7 +6,7 @@ do
     truncate -s 0 log.txt
     sleep 5
     screen -X -S $name hardcopy log.txt
-    if tail -n8 log.txt | grep '[Init] Current Config' || tail -n5 log.txt | grep 'Init Worker Master' || tail -n5 log.txt | grep 'Init Version Check' || tail -n5 log.txt | grep 'Claiming Master' || tail -n5 log.txt | grep 'stakeChecking' || tail -n5 log.txt | grep 'Could not read ConfigRegistry' || tail -n3 log.txt | grep 'sub routine initialized' || tail -n5 log.txt | grep 'Network seems to have stopped block production'   
+    if tail -n5 log.txt | grep 'Safety Sleep' || tail -n8 log.txt | grep '[Init] Current Config' || tail -n5 log.txt | grep 'Init Worker Master' || tail -n5 log.txt | grep 'Init Version Check' || tail -n5 log.txt | grep 'Claiming Master' || tail -n5 log.txt | grep 'stakeChecking' || tail -n5 log.txt | grep 'Could not read ConfigRegistry' || tail -n3 log.txt | grep 'sub routine initialized' || tail -n5 log.txt | grep 'Network seems to have stopped block production'   
     then
         echo "Restart worker $name. Worker Stuck";
         screen -r $name -X stuff $'\003'
