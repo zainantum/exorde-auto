e4af7f06e641df5528aa5bfdc75784f5d1fa4792
Zain Antum <115614618+zainantum@users.noreply.github.com>
2023-01-04 09:11:23 +0700

    Update autoRun.sh

--- autoRun.sh
+++ autoRun.sh
@@ -27,15 +27,15 @@ swapon /swapfile
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
-echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m2. Updating packages... \e[0m" && sleep 2
 sudo apt update && sudo apt install unzip -y
 
-echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m3. Installing dependencies... \e[0m" && sleep 2
 sudo apt install python3 python3-pip git screen -y && pip install --upgrade pip
 
 wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh && bash Anaconda3-2022.10-Linux-x86_64.sh -b -u
 
-echo -e "\e[1m\e[32m2. Cloning Exorde file... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m4. Cloning Exorde file... \e[0m" && sleep 2
 wget https://github.com/exorde-labs/ExordeModuleCLI/archive/refs/heads/main.zip \
 --output-document=ExordeModuleCLI.zip \
 && unzip ExordeModuleCLI.zip \
@@ -50,7 +50,7 @@ do
         sleep 1
 done
 
-echo -e "\e[1m\e[32m2. Create conda exorde enviroment... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m5. Create conda exorde enviroment... \e[0m" && sleep 2
 cd exorde
 source ~/anaconda3/etc/profile.d/conda.sh
 conda create --name exorde-env python=3.9 -y
@@ -58,7 +58,7 @@ conda activate exorde-env
 yes | pip install -r requirements.txt
 conda deactivate
 cd ..
-echo -e "\e[1m\e[32m2. Create screen... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m6. Create screen... \e[0m" && sleep 2
 
 array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
 for name in "${array[@]}"
@@ -69,15 +69,15 @@ do
         screen -r $name -X stuff 'cd '${name}' && source ~/anaconda3/etc/profile.d/conda.sh && conda activate exorde-env && python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
 done
 
-echo -e "\e[1m\e[32m1. Downloading auto restart... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m7. Downloading auto restart... \e[0m" && sleep 2
 wget https://raw.githubusercontent.com/zainantum/checker/main/c1.sh && chmod 777 c1.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/stuck.sh && chmod 777 stuck.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/updater.sh && chmod 777 updater.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/createScreen.sh && chmod 777 createScreen.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/swap.sh && chmod 777 swap.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/copyFile.sh && chmod 777 copyFile.sh
 
-echo -e "\e[1m\e[32m1. Add auto restart to crontab... \e[0m" && sleep 2
+echo -e "\e[1m\e[32m8. Add auto restart to crontab... \e[0m" && sleep 2
 pathFileRestart=$(realpath stuck.sh)
 pathFileRestart1=$(realpath c1.sh)
 crontab -l | { cat; echo "*/1 * * * * $pathFileRestart1"; } | crontab -
 crontab -l | { cat; echo "*/5 * * * * $pathFileRestart"; } | crontab -
 
 echo '=============== DONE ==================='
-echo -e "\e[1m\e[32m1. If auto restart doesnt exists in crontab, please report issue and add manually for now... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m If auto restart doesnt exists in crontab, please report issue and add manually for now... \e[0m" && sleep 1
 
