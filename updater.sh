83045004010e954f6ebdaf7ae54b997c1bb02ac2
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 06:43:16 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
-for pid in ${pidList[@]};
+rm -rf exorde*
+rm -rf c1.sh
+rm -rf stuck.sh
+rm -rf copyFile.sh
+rm -rf createScreen.sh
+wget https://raw.githubusercontent.com/zainantum/checker/main/c1.sh && chmod 777 c1.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/stuck.sh && chmod 777 stuck.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/updater.sh && chmod 777 updater.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/createScreen.sh && chmod 777 createScreen.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/swap.sh && chmod 777 swap.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/copyFile.sh && chmod 777 copyFile.sh
+wget https://github.com/exorde-labs/ExordeModuleCLI/archive/refs/heads/main.zip \
+--output-document=ExordeModuleCLI.zip \
+&& unzip ExordeModuleCLI.zip \
+&& rm ExordeModuleCLI.zip \
+&& mv ExordeModuleCLI-main exorde
+array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
+for name in "${array[@]}"
 do
-    #echo $pid;
-    name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
-    echo "Restart worker $name";
-    screen -r $name -X stuff $'\003'
-    screen -r $name -X stuff $'\003'
-    screen -r $name -X stuff $'\003'
-    sleep 5
-    screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+        echo "copy folder to $name"
+        cp -r exorde $name
+        sleep 1
+done
+if [ ! $mainAddress ]; then
+	read -p "Enter address for workers: " mainAddress
+	echo 'export mainAddress='$mainAddress >> $HOME/.bash_profile
+fi
+
+if [ ! $maxWorker ]; then
+        read -p "Enter maximum worker do you want: " maxWorker
+        echo 'export maxWorker='$maxWorker >> $HOME/.bash_profile
+fi
+
+source $HOME/.bash_profile
+
+for (( i=1; i<=$maxWorker; i++ ))
+do
+   name="exorde"$i
+   echo "create screen $name"
+   screen -dm $name
+   sleep 2
+   screen -r $name -X stuff 'cd '${name}' && source ~/anaconda3/etc/profile.d/conda.sh && conda activate exorde-env && python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
 done
