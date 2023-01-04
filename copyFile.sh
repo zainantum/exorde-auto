0bb6a2688db3bc7eb670f70bfb691600dfe92b87
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 06:01:02 +0700

    Update copyFile.sh

--- copyFile.sh
+++ copyFile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+rm -rf exorde*
+wget https://github.com/exorde-labs/ExordeModuleCLI/archive/refs/heads/main.zip \
+--output-document=ExordeModuleCLI.zip \
+&& unzip ExordeModuleCLI.zip \
+&& rm ExordeModuleCLI.zip \
+&& mv ExordeModuleCLI-main exorde
 array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
 for name in "${array[@]}"
 do
