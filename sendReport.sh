40fff9dab7776fbeeabbc8e5258a5da2980df59e
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-04 11:27:12 +0700

    Delete sendReport.sh

--- sendReport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
-for pid in ${pidList[@]};
-do
-    name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
-    screen -X -S $name hardcopy log.txt
-    mail -s $name emailbuatnyariuang@gmail.com < log.txt
-done
