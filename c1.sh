18bd03f8fde0dc877917f21c8982ca0cbfe6bb99
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 15:31:44 +0700

    Update c1.sh

--- c1.sh
+++ c1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source $HOME/.bash_profile
 pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
@@ -11,6 +12,6 @@ do
         echo "Worker $name is not working";
         #/usr/bin/screen -SX $pid quit;
 	      echo $name;
-	      screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+	      screen -r $name -X stuff 'python Launcher.py -m $mainAddress -l 3'`echo -ne '\015'`
     fi
 done
