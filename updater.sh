a5410c6e96a97104fc471db65b5aa7d22e4a5121
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 17:01:00 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -5,18 +5,6 @@ rm -rf stuck.sh
 rm -rf copyFile.sh
 rm -rf createScreen.sh
 wget https://raw.githubusercontent.com/zainantum/checker/main/c1.sh && chmod 777 c1.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/stuck.sh && chmod 777 stuck.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/updater.sh && chmod 777 updater.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/createScreen.sh && chmod 777 createScreen.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/swap.sh && chmod 777 swap.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/copyFile.sh && chmod 777 copyFile.sh
-wget https://github.com/exorde-labs/ExordeModuleCLI/archive/refs/heads/main.zip \
---output-document=ExordeModuleCLI.zip \
-&& unzip ExordeModuleCLI.zip \
-&& rm ExordeModuleCLI.zip \
-&& mv ExordeModuleCLI-main exorde
-array=( "exorde1" "exorde2" "exorde3" "exorde4" "exorde5" "exorde6" "exorde7" "exorde8" "exorde9" "exorde10" )
-for name in "${array[@]}"
-do
-        echo "copy folder to $name"
-        cp -r exorde $name
-        sleep 1
-done
 
 read -p "Enter address for workers: " mainAddress
 echo 'export mainAddress='$mainAddress >> $HOME/.bash_profile
@@ -25,6 +13,20 @@ read -p "Enter maximum worker do you want: " maxWorker
 echo 'export maxWorker='$maxWorker >> $HOME/.bash_profile
 
 source $HOME/.bash_profile
+
+wget https://github.com/exorde-labs/ExordeModuleCLI/archive/refs/heads/main.zip \
+--output-document=ExordeModuleCLI.zip \
+&& unzip ExordeModuleCLI.zip \
+&& rm ExordeModuleCLI.zip \
+&& mv ExordeModuleCLI-main exorde
+
+for (( i=1; i<=$maxWorker; i++ ))
+do
+   name="exorde"$i
+   echo "copy folder to $name"
+   cp -r exorde $name
+   sleep 1
+done
 
 for (( i=1; i<=$maxWorker; i++ ))
 do
