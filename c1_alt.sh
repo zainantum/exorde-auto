1e5a71b542a93703e4abad716ed3a688b87c77cc
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 22:35:58 +0700

    Create c1_alt.sh

---
+++ c1_alt.sh
@@ -0,0 +1,15 @@
+source $HOME/.bash_profile
+for (( i=1; i<=10; i++ ))
+do
+   name="exorde"$i
+   pid=$(ps aux | grep -w $name | grep -v grep | awk {'print $2'})
+   if pstree $pid | grep python
+    then
+        echo "Worker $name is working";
+    else
+        echo "Worker $name is not working";
+        #/usr/bin/screen -SX $pid quit;
+	      echo $name;
+	      screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
+    fi
+done
