5ac9176ca6502df5c3ce361e2f8a4b6a7dfa2b63
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-05 15:32:33 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source $HOME/.bash_profile
 pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
@@ -22,7 +23,7 @@ do
         screen -r $name -X stuff $'\003'
         screen -r $name -X stuff $'\003'
         sleep 15
-        screen -r $name -X stuff 'python Launcher.py -m 0x80bE97A5580061a647bb04ADaeb8d18fe963ae55 -l 3'`echo -ne '\015'`
+        screen -r $name -X stuff 'python Launcher.py -m $mainAddress -l 3'`echo -ne '\015'`
     else
         echo "Worker $name running perfectly";
     fi
