6f96591453600ef4c1ccef9094cf19f76c6e83f4
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-06 21:10:02 +0700

    Update createScreen.sh

--- createScreen.sh
+++ createScreen.sh
@@ -11,6 +11,7 @@ for (( i=11; i<=$maxWorker; i++ ))
 do
    name="exorde"$i
    echo "create screen $name"
+   cp -r exorde $name
    screen -dm $name
    sleep 2
    screen -r $name -X stuff 'cd '${name}' && source ~/anaconda3/etc/profile.d/conda.sh && conda activate exorde-env && python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
