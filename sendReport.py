68eb7351ab7d79bc58738fc260d1f2f85ff66dc6
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-07 16:16:47 +0700

    Update sendReport.py

--- sendReport.py
+++ sendReport.py
@@ -3,12 +3,13 @@ import sys
 
 worker = sys.argv[1]
 hostname = sys.argv[2]
+idchat = sys.argv[3]
 #please change you chatID with yours
 
 def send_to_telegram(message):
 
     apiToken = '5717312084:AAFYl4qrYosNPojHPy6MWZb8W16ab_fR0VI'
-    chatID = '461229417'
+    chatID = idchat
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
 
     try:
