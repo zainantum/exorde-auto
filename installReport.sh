47c1c3d9fdc37fad1468b4fd4e643b6a1a966576
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-07 14:55:26 +0700

    Create installReport.sh

---
+++ installReport.sh
@@ -0,0 +1,36 @@
+
+clear
+echo -e "\e[35m"
+echo "====================================================================================" 
+
+echo -e '\e[35mNode :\e[35m' Exorde
+echo -e '\e[35mTelegram :\e[35m' @imrnmln
+echo -e '\e[35mTwitter :\e[35m' @zainantum
+echo -e '\e[35mDiscord :\e[35m' @imrnmln#7847
+echo "===================================================================================="
+sleep 2
+
+if [ ! $chatid ]; then
+	read -p "Enter your Chat ID: " chatid
+	echo 'export chatid='$chatid >> $HOME/.bash_profile
+fi
+
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Your Chat ID: \e[1m\e[32m$chatid\e[0m"
+echo '================================================='
+sleep 2
+echo -e "\e[1m\e[32m1. Downloading auto send log... \e[0m" && sleep 2
+rm -rf sendLog.sh
+rm -rf sendReport.sh
+
+wget https://raw.githubusercontent.com/zainantum/checker/main/sendReport.sh && chmod 777 sendReport.sh && wget https://raw.githubusercontent.com/zainantum/checker/main/sendLog.sh && chmod 777 sendLog.sh
+pathFileRestart=$(realpath sendLog.sh)
+
+crontab -l > mycron
+echo "0 * * * * $pathFileRestart" >> mycron
+crontab mycron
+rm mycron
+
+echo -e "\e[1m\e[32m2. Successfully ... \e[0m" && sleep 2
