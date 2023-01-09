4d6e8c2c1894e78ccef7e557f3e48d5650ffaa53
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 05:56:39 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -24,7 +24,7 @@ do
         screen -r $name -X stuff $'\003'
         sleep 15
         screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
-    elif (tail -n45 log.txt | grep 'No new work' | wc -l) > "2" || tail -n5 log.txt | grep 'python: not found' || tail -n5 log.txt | grep 'sub routine initialized' || tail -n20 log.txt | grep 'Initialization error Something went' || tail -n20 log.txt | grep 'Read timed out'
+    elif (tail -n45 log.txt | grep 'No new work' | wc -l) > "5" || tail -n5 log.txt | grep 'python: not found' || tail -n5 log.txt | grep 'sub routine initialized' || tail -n20 log.txt | grep 'Initialization error Something went' || tail -n20 log.txt | grep 'Read timed out'
     then
         echo "Worker stuck too long. Re-install worker"
         kill -9 $pid
