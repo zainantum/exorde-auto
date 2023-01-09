2a04298daaeac2403e4b51f85c642d45aaf5ad48
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-09 18:14:28 +0700

    Update changeMaxWorker.sh

--- changeMaxWorker.sh
+++ changeMaxWorker.sh
@@ -1,6 +1,6 @@
 0#!/bin/bash
-if [ -n "$maxWorker " ]; then
-  sed -i -e "s|maxWorker*=.*|maxWorker =$1|" $HOME/.bash_profile
+if [ -n "$maxWorker" ]; then
+  sed -i -e "s|maxWorker*=.*|maxWorker=$1|" $HOME/.bash_profile
 else
-  echo 'export maxWorker ='$1 >> $HOME/.bash_profile
+  echo 'export maxWorker='$1 >> $HOME/.bash_profile
 fi
