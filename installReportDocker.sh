a51632169e16115128fd2bb1742cacfec5326236
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-09 11:13:13 +0700

    Create installReportDocker.sh

---
+++ installReportDocker.sh
@@ -0,0 +1,41 @@
+source $HOME/.bash_profile
+clear
+echo -e "\e[35m"
+echo "====================================================================================" 
+
+echo -e '\e[35mNode :\e[35m' Exorde Send Log Docker
+echo -e '\e[35mTelegram :\e[35m' @imrnmln
+echo -e '\e[35mTwitter :\e[35m' @zainantum
+echo -e '\e[35mDiscord :\e[35m' @imrnmln#7847
+echo "===================================================================================="
+sleep 2
+
+if [ ! $chatid ]; then
+	read -p "Enter your Chat ID: " chatid
+	echo 'export chatid='$chatid >> $HOME/.bash_profile
+fi
+
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Your Chat ID: \e[1m\e[32m$chatid\e[0m"
+echo '================================================='
+sleep 2
+echo -e "\e[1m\e[32m1. Downloading auto send log... \e[0m" && sleep 2
+rm -rf sendLogDocker.sh
+rm -rf sendReport.sh
+
+wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sendReport.py && chmod 777 sendReport.py && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sendLogDocker.sh && chmod 777 sendLogDocker.sh
+sleep 2
+pathFileRestart=$(realpath sendLogDocker.sh)
+
+if ! crontab -l | grep -q 'sendLog';
+then
+    echo "Add script to cronjob"
+    crontab -l > mycron
+    echo "0 * * * * $pathFileRestart" >> mycron
+    crontab mycron
+    rm mycron
+fi
+
+echo -e "\e[1m\e[32m2. Successfully ... \e[0m" && sleep 2
