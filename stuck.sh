2752a27d2660e753bbf381d04ba2c6eeccbb3b70
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 09:26:08 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source $HOME/.bash_profile
-pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
+pidList=$(screen -ls | grep "exorde" | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
     name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
