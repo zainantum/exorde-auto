afd7adb9542aa1eea6eaa136bfc3be0b572e4af5
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 09:27:06 +0700

    Update c1.sh

--- c1.sh
+++ c1.sh
@@ -3,15 +3,12 @@ source $HOME/.bash_profile
 pidList=$(screen -ls | grep "exorde" | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
-    #echo $pid;
     name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
     if pstree $pid | grep python
     then
         echo "Worker $name is working";
     else
         echo "Worker $name is not working";
-        #/usr/bin/screen -SX $pid quit;
-	      echo $name;
-	      screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
+	screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
     fi
 done
