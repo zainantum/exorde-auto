36acd19843a9fc77586cb58922408c2405774b8e
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 17:00:08 +0700

    Update sendErrLog.sh

--- sendErrLog.sh
+++ sendErrLog.sh
@@ -9,7 +9,7 @@ do
     screen -X -S $name hardcopy logErr1.txt
     if tail -n10 logErr1.txt | grep 'Initialization error Something went' || tail -n20 logErr1.txt | grep 'Read timed out' || tail -n5 logErr1.txt | grep 'Safety Sleep' || tail -n8 logErr1.txt | grep '[Init] Current Config' || tail -n5 logErr1.txt | grep 'Init Worker Master' || tail -n5 logErr1.txt | grep 'Init Version Check' || tail -n5 logErr1.txt | grep 'Claiming Master' || tail -n5 logErr1.txt | grep 'stakeChecking' || tail -n5 logErr1.txt | grep 'Could not read ConfigRegistry' || tail -n5 logErr1.txt | grep 'Network seems to have stopped block production'   
     then
-        tail -n10 logErr1.txt > logErr2.txt
+        tail -n10 logErr1.txt > log1.txt
         python3 sendReport.py $name `hostname -s` $chatid
         sleep 2
     fi        
