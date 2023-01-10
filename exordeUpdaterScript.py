d7b0696a2ad802301655177e76c67fe238d7fdc8
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 08:14:24 +0700

    Update exordeUpdaterScript.py

--- exordeUpdaterScript.py
+++ exordeUpdaterScript.py
@@ -1,22 +1,48 @@
 import paramiko
 
 
-def switch(num, ip, un, pw):
+def switch(num, ch1, ch2, ip, un, pw):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=un, password=pw)
     print("Updatingg....."+num)
-    stdin, stdout, stderr = client.exec_command('rm -rf '+num+' && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/'+num+' && chmod 777 '+num)
-    stdout=stdout.readlines()
-    stdin.close()
-    print(stdout)
-    print("Successfully")
+    if num == "1":
+            chl = int(ch2)
+            if chl>0:
+                  for i in range(chl): 
+                       stdin, stdout, stderr = client.exec_command(ch1)
+            else:
+                 stdin, stdout, stderr = client.exec_command(ch1)
+            stdout=stdout.readlines()
+            stdin.close()
+            print(stdout)
+            print("Successfully")
+    elif num == "2":
+            if ch2 == "y":
+                        print("Executing... "+ch1)
+                        ch2 = " && ./"+ch1
+            else:
+                 ch2 = ""
+            print("Check command "+ch2)
+            stdin, stdout, stderr = client.exec_command('rm -rf '+ch1+' && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/'+ch1+' && chmod 777 '+ch1+ch2)
+            stdout=stdout.readlines()
+            stdin.close()
+            print(stdout)
+            print("Successfully")
+    elif num == "3":
+            print("Skipped")
 
     
-ch = input("Please Enter File Name (exp stuck.sh): ")
-        
+print("Choose what you want to do:\n 1. Execute script \n 2. Update and execute script \n 3. skip")
+ch = input("Please Enter Your Choice: ")   
+if ch == "1":
+            cha = input("Please Enter Command: ")   
+            chb = input("Looping this command? if yes input max looping, if no input 0 ")
+elif ch == "2":
+            cha = input("Please Enter Command Do You Want to Update: ")
+            chb = input("run script? y or n ")
 with open('listIp.txt') as f:
    for line in f:
        data = line.split(";")
        print("Accessing "+str(data[0]))
-       switch(ch, data[0],data[1],data[2])
+       switch(ch, cha, chb, data[0],data[1],data[2])
