040b4a02eb4a4ec3a4ba3cea6921d900eba10afd
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 10:30:51 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -7,9 +7,21 @@ do
     truncate -s 0 log.txt
     sleep 5
     screen -X -S $name hardcopy log.txt
-    if (tail -n30 log.txt | grep 'No new work' | wc -l) > "10" || tail -n3 log.txt | grep 'sub routine initialized' || tail -n10 log.txt | grep 'Initialization error Something went' || tail -n20 log.txt | grep 'Read timed out' || tail -n5 log.txt | grep 'Safety Sleep' || tail -n8 log.txt | grep '[Init] Current Config' || tail -n5 log.txt | grep 'Init Worker Master' || tail -n5 log.txt | grep 'Init Version Check' || tail -n5 log.txt | grep 'Claiming Master' || tail -n5 log.txt | grep 'stakeChecking' || tail -n5 log.txt | grep 'Could not read ConfigRegistry' || tail -n5 log.txt | grep 'Network seems to have stopped block production'   
+    if (tail -n30 log.txt | grep 'No new work' | wc -l) > "10" || tail -n10 log.txt | grep 'Initialization error Something went' || tail -n20 log.txt | grep 'Read timed out' || tail -n5 log.txt | grep 'Safety Sleep' || tail -n8 log.txt | grep '[Init] Current Config' || tail -n5 log.txt | grep 'Init Worker Master' || tail -n5 log.txt | grep 'Init Version Check' || tail -n5 log.txt | grep 'Claiming Master' || tail -n5 log.txt | grep 'stakeChecking' || tail -n5 log.txt | grep 'Could not read ConfigRegistry' || tail -n5 log.txt | grep 'Network seems to have stopped block production'   
     then
         echo "Restart worker $name. Worker Stuck";
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
         screen -r $name -X stuff $'\003'
         screen -r $name -X stuff $'\003'
         screen -r $name -X stuff $'\003'
