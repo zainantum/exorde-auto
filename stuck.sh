280f592f8c3fbdea0633148929cf35348962a2d0
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-10 09:33:41 +0700

    Update stuck.sh

--- stuck.sh
+++ stuck.sh
@@ -24,6 +24,15 @@ do
         screen -r $name -X stuff $'\003'
         sleep 15
         screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
+    elif (tail -n45 log.txt | grep 'No new work' | wc -l) > "10" || tail -n5 log.txt | grep 'python: not found' || tail -n5 log.txt | grep 'sub routine initialized' || tail -n20 log.txt | grep 'Initialization error Something went' || tail -n20 log.txt | grep 'Read timed out'
+    then
+        echo "Worker stuck too long. Re-install worker"
+        screen -X -S $name quit
+        sleep 2
+        echo "Close $name screen done"
+        screen -dm $name
+        sleep 2
+        screen -r $name -X stuff 'cd '${name}' && source ~/anaconda3/etc/profile.d/conda.sh && conda activate exorde-env && python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
     else
         echo "Worker $name running perfectly";
     fi
