e496a58b103a8878b1a4311cd932b2248072a6fe
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-11 15:51:58 +0700

    Update sendLog.sh

--- sendLog.sh
+++ sendLog.sh
@@ -4,12 +4,12 @@ pidList=$(screen -ls | awk '/[0-9]{3,}\./ {print strtonum($1)}')
 for pid in ${pidList[@]};
 do
     name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
-    truncate -s 0 log.txt
+    truncate -s 0 pathreplace/log.txt
     sleep 1
     echo "Log worker $name";
-    screen -X -S $name hardcopy log.txt
+    screen -X -S $name hardcopy pathreplace/log.txt
     sleep 2
-    tail -n7 log.txt > log1.txt
+    tail -n7 pathreplace/log.txt > pathreplace/log1.txt
     python3 pathreplace/sendReport.py $name `hostname -s` $chatid
     sleep 2
 done
