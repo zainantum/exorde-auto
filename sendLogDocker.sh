0735301ecb12cfcd019bfd60ab99a013795fb765
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-18 12:08:43 +0700

    Update sendLogDocker.sh

--- sendLogDocker.sh
+++ sendLogDocker.sh
@@ -3,9 +3,8 @@ source $HOME/.bash_profile
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-    truncate -s 0 pathreplace/log2.txt
-    docker logs $pid >> pathreplace/log2.txt
-    tail -n15 pathreplace/log2.txt > pathreplace/log1.txt
+    truncate -s 0 pathreplace/log1.txt
+    docker logs $pid -t --tail=50 > pathreplace/log1.txt
     sleep 1
     python3 pathreplace/sendReport.py $pid `hostname -s` $chatid
 done
