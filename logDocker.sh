8a9407c0655b827322425bda727b6c761576d83a
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-18 12:05:43 +0700

    Update logDocker.sh

--- logDocker.sh
+++ logDocker.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-source $HOME/.bash_profile
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-    truncate -s 0 log.txt
-    docker logs -f $pid -t --tail=25 >> log.txt
-    sleep 1
     echo "Log worker $pid"
-    tail -n7 log.txt
-    sleep 2
+    docker logs $pid -t --tail=50
+    sleep 1
 done
