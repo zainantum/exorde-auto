1eed4d5cb60e576c4f1ff23aece69fe46911914c
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-20 16:20:38 +0700

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-  if docker logs $pid -t --tail=25 | grep "ERROR" || docker logs $pid -t --tail=25 | grep "Init Version Check";
+  if docker logs $pid -t --tail=1 | grep "Init Version Check";
   then 
       echo "Worker $pid stuck. Restarting worker";
       docker stop $pid
