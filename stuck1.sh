74d171b6c8f53e3b23a05e70b5e5e1f5bc190a91
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-21 11:09:01 +0700

    Create stuck1.sh

---
+++ stuck1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source $HOME/.bash_profile
+pidList=$(screen -ls | grep "exorde" | awk '/[0-9]{3,}\./ {print strtonum($1)}')
+for pid in ${pidList[@]};
+do
+    name=$(screen -ls | grep $pid | awk '{print $1}' | cut -d. -f 2)
+    truncate -s 0 log.txt
+    sleep 5
+    screen -X -S $name hardcopy log.txt
+    if tail -n5 log.txt | grep 'Init Worker Master' || tail -n2 log.txt | grep 'Claiming Master'  
+    then
+        echo "Restart worker $name. Worker Stuck";
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        screen -r $name -X stuff $'\003'
+        sleep 15
+        screen -r $name -X stuff 'python Launcher.py -m '${mainAddress}' -l 3'`echo -ne '\015'`
+    else
+        echo "Worker $name running perfectly";
+    fi
+    
+        
+done
