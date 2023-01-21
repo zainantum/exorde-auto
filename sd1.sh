8f3fe1370b63ba586745e0a268ac75c19b5d4dcc
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-21 11:40:23 +0700

    Create sd1.sh

---
+++ sd1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
+for pid in ${pidList[@]};
+do
+  if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure";
+  then 
+      echo "Worker $pid stuck. Restarting worker";
+      docker stop $pid && docker start $pid
+  fi
+done
