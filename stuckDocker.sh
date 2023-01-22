00b9fbef058edf109a4a4431490f0f3c02ed9487
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-22 10:48:07 +0700

    Update stuckDocker.sh

--- stuckDocker.sh
+++ stuckDocker.sh
@@ -3,7 +3,7 @@ source $HOME/.bash_profile
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-    if docker logs $pid -t --tail=3 | grep 'Initialization error Something went' || docker logs $pid -t --tail=3 | grep 'Read timed out' || docker logs $pid -t --tail=3 | grep 'Safety Sleep' || docker logs $pid -t --tail=3 | grep 'Could not read ConfigRegistry' || docker logs $pid -t --tail=3 | grep 'Network seems to have stopped block production'   
+    if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure" || docker logs $pid -t --tail=3 | grep 'Initialization error Something went' || docker logs $pid -t --tail=3 | grep 'Read timed out' || docker logs $pid -t --tail=3 | grep 'Safety Sleep' || docker logs $pid -t --tail=3 | grep 'Could not read ConfigRegistry' || docker logs $pid -t --tail=3 | grep 'Network seems to have stopped block production'   
     then
         echo "Worker $pid stuck. Restarting worker";
         docker stop $pid && docker start $pid
