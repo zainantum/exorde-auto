b68b70023e561f5fe78ad39adbfa4aeb8555317c
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-31 20:33:38 +0700

    Update sd1.sh

--- sd1.sh
+++ sd1.sh
@@ -2,7 +2,7 @@
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-  if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure";
+  if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure" || docker logs $pid -t --tail=5 | grep "Safety Sleep"
   then 
       echo "Worker $pid stuck. Restarting worker";
       docker restart $pid
