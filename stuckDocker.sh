d2ada7153f0646859d359ce5630fbdd31774ec33
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-31 13:27:21 +0700

    Update stuckDocker.sh

--- stuckDocker.sh
+++ stuckDocker.sh
@@ -6,7 +6,7 @@ do
     if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure" || docker logs $pid -t --tail=3 | grep 'Initialization error Something went' || docker logs $pid -t --tail=3 | grep 'Read timed out' || docker logs $pid -t --tail=3 | grep 'Safety Sleep' || docker logs $pid -t --tail=3 | grep 'Could not read ConfigRegistry' || docker logs $pid -t --tail=3 | grep 'Network seems to have stopped block production'   
     then
         echo "Worker $pid stuck. Restarting worker";
-        docker stop $pid && docker start $pid
+        docker restart $pid
     else
         echo "Worker $pid running perfectly";
     fi
