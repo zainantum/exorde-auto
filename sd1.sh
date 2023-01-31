fa2c6023acafb3596d2c127ff8b6da8f6586d4d7
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-01-31 21:33:05 +0700

    Update sd1.sh

--- sd1.sh
+++ sd1.sh
@@ -2,7 +2,7 @@
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-  if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure" || docker logs $pid -t --tail=3 | grep 'Initialization error Something went' || docker logs $pid -t --tail=3 | grep 'Safety Sleep' || docker logs $pid -t --tail=3 | grep 'Could not read ConfigRegistry' || docker logs $pid -t --tail=3 | grep 'Network seems to have stopped block production'
+  if docker logs $pid -t --tail=1 | grep "Auto-Faucet critical Failure" 
   then 
       echo "Worker $pid stuck. Restarting worker";
       docker restart $pid
