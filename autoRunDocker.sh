3cacfdf66ac29575c3054b650ff7daba40f1c40b
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-02-03 05:12:25 +0700

    Update autoRunDocker.sh

--- autoRunDocker.sh
+++ autoRunDocker.sh
@@ -62,7 +62,7 @@ echo -e "\e[1m\e[32m3. Downloading auto restart and report... \e[0m" && sleep 2
 wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/installReportDocker.sh && chmod 777 installReportDocker.sh && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sendLogDocker.sh && chmod 777 sendLogDocker.sh && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sendReport.py && chmod 777 sendReport.py && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/stuckDocker.sh && chmod 777 stuckDocker.sh && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/updaterDocker.sh && chmod 777 updaterDocker.sh && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/logDocker.sh && chmod 777 logDocker.sh
 wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sd.sh && chmod +x sd.sh
 wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/sde.sh && chmod +x sde.sh
-
+wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/createWorkerDocker.sh && chmod +x createWorkerDocker.sh
 echo -e "\e[1m\e[32m4. Add auto restart to cronjob... \e[0m" && sleep 2
 pathFileRestart=$(realpath stuckDocker.sh)
 pathFileRestart1=$(realpath sd.sh)
