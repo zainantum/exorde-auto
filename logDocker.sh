53b12d9e9f3884005ea77600c0e71c9caebc5bcc
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-21 12:18:05 +0700

    Update logDocker.sh

--- logDocker.sh
+++ logDocker.sh
@@ -2,7 +2,12 @@
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-    echo "Log worker $pid"
-    docker logs $pid -t --tail=20
+    echo "Log worker $pid: REP"
+    docker logs $pid 2>&1 | grep "REP"
     sleep 1
+    echo "Log worker $pid: new item"
+    docker logs $pid 2>&1 | grep "new item"
+    sleep
+    echo "Log worker $pid: crashed"
+    docker logs $pid 2>&1 | grep "crashed"
 done
