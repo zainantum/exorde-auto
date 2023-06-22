99f736116e37dfab8e3ed6e3745134f48ed2dcca
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-22 21:49:21 +0700

    Create workerLog.py

---
+++ workerLog.py
@@ -0,0 +1,22 @@
+import paramiko
+
+
+def switch():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=un, password=pw)
+    print("Accessing vps....."+num)
+    for i in range(5):
+      stdin, stdout, stderr = client.exec_command('docker logs -t --tail=100 exorde'+i)
+      stdout=stdout.readlines()
+    stdin.close()
+    print(stdout)
+    print("Successfully")
+
+    
+print("Pastikan ip dan password vps benar") 
+with open('listIp.txt') as f:
+   for line in f:
+       data = line.split(";")
+       print("Accessing "+str(data[0]))
+       switch()
