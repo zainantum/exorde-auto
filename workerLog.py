d629862742afc524486cf0e8eb91cbe2b728d4d3
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-22 22:10:40 +0700

    Update workerLog.py

--- workerLog.py
+++ workerLog.py
@@ -5,12 +5,14 @@ def switch(ip, un, pw):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=un, password=pw)
-    print("Accessing vps....."+num)
+    print("Accessing vps.....")
     for i in range(5):
-      stdin, stdout, stderr = client.exec_command('docker logs -t --tail=100 exorde'+i)
-      stdout=stdout.readlines()
-    stdin.close()
-    print(stdout)
+        stdin, stdout, stderr = client.exec_command('docker logs -t --tail=100 exorde'+str(i))
+        stdout=stdout.readlines()
+        print(stdout)
+        stdin.close()
+        sleep(5)
+        
     print("Successfully")
 
     
