f1ed6273866f4d6f853200f440932f4c8fb5a629
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-23 10:28:07 +0700

    Update workerLog.py

--- workerLog.py
+++ workerLog.py
@@ -1,19 +1,54 @@
 import docker
 import sys
+import mysql.connector
 
 worker = int(sys.argv[1])
 hostname = sys.argv[2]
 address = sys.argv[3]
 
-client = docker.from_env()
-for container_name in range(1,worker+1):
-    dkg = client.containers.get("exorde"+str(container_name)).logs(stream = True, follow = False, tail=3)
+def insertData(ip, db, us, pw, tableName, fieldName, value):
     try:
-      while True:
-        line = next(dkg).decode("utf-8")
-        print(line)
-    except StopIteration:
-      print(f'log stream ended for exorde{container_name}')
+        connection = mysql.connector.connect(host=ip,
+                                            database=db,
+                                            user=us,
+                                            password=pw)
 
+        mySql_insert_query = """INSERT INTO """+str(tableName)+""" ("""+str(fieldName)+""") 
+                            VALUES 
+                            ("""+str(value)+""") """
+        print(mySql_insert_query)
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        # print(cursor.rowcount, "Record inserted successfully into Laptop table")
+        cursor.close()
+
+    except mysql.connector.Error as error:
+        print("Failed to insert record into Laptop table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
+
+def getLog(ip, db, us, pw):
+    client = docker.from_env()
+    for container_name in range(1,worker+1):
+        dkg = client.containers.get("exorde"+str(container_name)).logs(stream = True, follow = False, tail=3)
+        try:
+          while True:
+            line = next(dkg).decode("utf-8")
+            print(line)
+            field = "address, hostname, log, container_id"
+            value = address + "," + hostname + "," + line + ","+"exorde"+str(container_name)
+            insertData(ip, db, us, pw, "tb_d_log", field, value)
+        except StopIteration:
+          print(f'log stream ended for exorde{container_name}')
+
+
+with open('listDB.txt') as f:
+   for line in f:
+       data = line.split(";")
+       getLog(data[0],data[1],data[2],data[3])
 
 
