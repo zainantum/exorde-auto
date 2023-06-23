5e42f6e44004791cf440d802ffeb8e870cea98ee
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-23 10:03:36 +0700

    Create temporaryLog.sh

---
+++ temporaryLog.sh
@@ -0,0 +1,25 @@
+clear
+echo -e "\e[35m"
+echo "====================================================================================" 
+
+echo -e '\e[35mNode :\e[35m' Exorde
+echo -e '\e[35mTelegram :\e[35m' @imrnmln
+echo -e '\e[35mTwitter :\e[35m' @zainantum
+echo -e '\e[35mDiscord :\e[35m' @imrnmln#7847
+echo "===================================================================================="
+sleep 2
+
+read -p "Enter your total worker in this vm: " worker
+read -p "Enter your address for log: " address
+
+echo -e "\e[1m\e[32m7. Add auto logging to cron job... \e[0m" && sleep 2
+rm -rf workerLog* && wget https://raw.githubusercontent.com/zainantum/exorde-auto/main/workerLog.py && chmod +x *
+if ! crontab -l | grep -q 'workerLog';
+then
+    echo "Add script to cronjob"
+    crontab -l > mycron
+    echo "*/30 * * * * python3 workerLog.py $worker `hostname` $address" >> mycron
+    crontab mycron
+    rm mycron
+fi
+echo '=============== DONE ==================='
