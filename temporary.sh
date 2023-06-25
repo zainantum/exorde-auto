30890a3de1c2fc6d5650cf00cd7792a22a36c7e8
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-26 06:04:44 +0700

    Update temporary.sh

--- temporary.sh
+++ temporary.sh
@@ -69,6 +69,8 @@ do
    echo "copy container $name"
    if [ $useTwitter == "y" ]; then
    	docker run -d --memory=8g --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress --twitter_username '${ust}' --twitter_password '${pwt}' --twitter_email '${mailt}'
+   elif [ $useTwitter == "c" ]; then
+        docker run -d --cpus="2" --memory="8g" --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress
    else
         docker run -d --memory=8g --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress
    fi
