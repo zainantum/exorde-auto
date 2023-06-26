98dfd7f71574c195b8994819e94319d68df2d81f
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-26 10:02:40 +0700

    Update temporary.sh

--- temporary.sh
+++ temporary.sh
@@ -71,6 +71,8 @@ do
    	docker run -d --memory=8g --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress --twitter_username '${ust}' --twitter_password '${pwt}' --twitter_email '${mailt}'
    elif [ $useTwitter == "c" ]; then
         docker run -d --cpus="2" --memory="8g" --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress
+   elif [ $useTwitter == "d" ]; then
+        docker run -d --cpus="4" --memory="16g" --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress
    else
         docker run -d --memory=8g --restart unless-stopped --pull always --name $name exordelabs/exorde-client --main_address $mainAddress
    fi
