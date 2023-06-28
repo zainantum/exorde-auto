0a10534924c7b522f06439362f1c4b516e7d558e
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-06-28 13:57:33 +0700

    Update changeSwap.sh

--- changeSwap.sh
+++ changeSwap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Create swap? y or n " makeSwap
+read -p "Input new swap size (in megabyte): " makeSwap
 swapoff -a
 dd if=/dev/zero of=/swapfile bs=1M count=$makeSwap
 mkswap /swapfile
