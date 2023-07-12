0a01e5b8284e1a6bb12060e7a1ee287ca3bbdb67
Zain Antum a.k.a imrnmln <115614618+zainantum@users.noreply.github.com>
2023-07-12 09:27:15 +0700

    Update logDocker.sh

--- logDocker.sh
+++ logDocker.sh
@@ -2,8 +2,6 @@
 pidList=$(docker ps --filter "name=exor" --format "{{.Names}}")
 for pid in ${pidList[@]};
 do
-    echo "Log worker $pid: REP"
-    docker logs $pid 2>&1 | grep "REP"
     sleep 1
     echo "Log worker $pid: new item"
     docker logs $pid -t --tail=50
